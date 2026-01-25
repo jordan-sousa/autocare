5bedffcdb6290b7f9b3332f9557fa978da54ef6c
Jordan Sousa <jordan.sousajc@gmail.com>
2026-01-24 23:19:26 -0300

    first commit

---
+++ src/main/java/com/jordan/autocare/AutocareApplication.java
@@ -0,0 +1,13 @@
+package com.jordan.autocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordan/autocare/AutocareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.autocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
