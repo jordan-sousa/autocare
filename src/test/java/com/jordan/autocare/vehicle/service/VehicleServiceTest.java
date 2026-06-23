3bc7b6be96b2271349855c756ae40312157a2f80
Jordan Sousa <jordan.sousajc@gmail.com>
2026-06-22 22:21:16 -0300

    implementing tests in the project

---
+++ src/test/java/com/jordan/autocare/vehicle/service/VehicleServiceTest.java
@@ -0,0 +1,47 @@
+package com.jordan.autocare.vehicle.service;
+
+import com.jordan.autocare.auth.repository.UserRepository;
+import com.jordan.autocare.vehicle.domain.Vehicle;
+import com.jordan.autocare.vehicle.dto.VehicleResponse;
+import com.jordan.autocare.vehicle.repository.VehicleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class VehicleServiceTest {
+
+    @Mock
+    private VehicleRepository vehicleRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private VehicleService vehicleService;
+
+    @Test
+    void shoudFindVehicleById() {
+        Vehicle vehicle = Vehicle.builder()
+                .id(1L)
+                .brand("Fiat")
+                .model("Mobi")
+                .year(2018)
+                .currentMileage(135000)
+                .build();
+
+        when(vehicleRepository.findById(1L))
+                .thenReturn(Optional.of(vehicle));
+
+        VehicleResponse response = vehicleService.findById(1L);
+
+        assertEquals("Fiat", response.brand());
+    }
+}
